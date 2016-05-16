c0a0470c978d71e332b968d0edd931242546352b
dipesh.dhakal <dboy.gilchrist@gmail.com>
2016-05-16 13:17:51 +0545

    Removing Facebook Stuffs

--- app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
+++ app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
@@ -1,31 +1,15 @@
 package com.bipin.android.socialmedialogindemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
-    private LoginButton loginButton;
-    private CallbackManager callbackManager;
-    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbarAndfloatingActionBottom();
-        faceBookLoginListener();
 
     }
 
@@ -52,68 +35,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    public void onFacebookLoginBottomClick(View view){
-
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.e(MainActivity.class.getSimpleName() , loginResult.toString());
-            }
-
-            @Override
-            public void onCancel() {
-                Log.e(MainActivity.class.getSimpleName() , "onCancel");
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                Log.e(MainActivity.class.getSimpleName() , exception.toString());
-            }
-        });
-    }
-
-    private void faceBookLoginListener(){
-
-        callbackManager = CallbackManager.Factory.create();
-        accessTokenTracker = new AccessTokenTracker() {
-            @Override
-            protected void onCurrentAccessTokenChanged(
-                    AccessToken oldAccessToken,
-                    AccessToken currentAccessToken) {
-                Log.e(MainActivity.class.getSimpleName() , oldAccessToken.getToken() + " " + currentAccessToken.getToken());
-
-            }
-        };
-
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        String accessToken = loginResult.getAccessToken().getToken();
-                        Log.e(MainActivity.class.getSimpleName() , loginResult.toString());
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Log.e(MainActivity.class.getSimpleName() , "Cancel");
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Log.e(MainActivity.class.getSimpleName() , exception.toString());
-                    }
-                });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        Log.e(MainActivity.class.getSimpleName() , data.toString());
     }
 
 }
