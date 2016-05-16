ce3bd06c2f0db12639d7b8d0ffb4e0939ee12a82
dipesh.dhakal <dboy.gilchrist@gmail.com>
2016-05-16 13:46:52 +0545

    KeyHash

--- app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
+++ app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
@@ -1,11 +1,15 @@
 package com.bipin.android.socialmedialogindemo;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 
@@ -15,10 +19,14 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
     private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         toolbarAndfloatingActionBottom();
         faceBookLoginListener();
-
+        generateFacebookKeyHash();
     }
 
 
@@ -88,6 +97,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+                Log.e(MainActivity.class.getSimpleName() , oldProfile.getFirstName() + " " + currentProfile.getFirstName());
+            }
+        };
+
         LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
@@ -109,6 +127,26 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    private void generateFacebookKeyHash(){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.bipin.android.socialmedialogindemo",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String keyHash =  Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -116,4 +154,10 @@ public class MainActivity extends AppCompatActivity {
         Log.e(MainActivity.class.getSimpleName() , data.toString());
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
 }
