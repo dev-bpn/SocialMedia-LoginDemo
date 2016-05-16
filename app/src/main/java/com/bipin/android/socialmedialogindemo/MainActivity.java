4470e75628a176426022702657744e51190e8358
dipesh.dhakal <dboy.gilchrist@gmail.com>
2016-05-16 17:45:09 +0545

    Retrieve Basic Profile

--- app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
+++ app/src/main/java/com/bipin/android/socialmedialogindemo/MainActivity.java
@@ -1,40 +1,107 @@
 package com.bipin.android.socialmedialogindemo;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
-import com.facebook.FacebookSdk;
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main);
+        generateKeyHash();
 
-        toolbarAndfloatingActionBottom();
 
     }
 
+    public void onLoginClick(View v){
 
-    private void toolbarAndfloatingActionBottom(){
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onAuthSuccess() {
+                // Authentication was successful.  You can now do
+                // other calls with the SDK.
+                Log.e("TAG" , "");
+                retriveBasicProfile();
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+                Log.e("TAG" , error.toString());
+            }
+        }, true);
+
+    }
+
+    private void retriveBasicProfile(){
+
+        String url = "https://api.linkedin.com/v1/people/~";
+
+        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        apiHelper.getRequest(this, url, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                Log.e("TAG" , apiResponse.toString());
+            }
+
+            @Override
+            public void onApiError(LIApiError liApiError) {
+                Log.e("TAG" , liApiError.toString());
             }
         });
 
     }
 
+    // Build the list of member permissions our LinkedIn session requires
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Add this line to your existing onActivityResult() method
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+    }
+
+
+    private void generateKeyHash(){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.bipin.android.socialmedialogindemo",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String keyHash =  Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
